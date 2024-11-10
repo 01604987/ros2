b05742cbd3ef6aba4c5db715ff1c08c861a8b88f
01604987 <e1604987@student.tuwien.ac.at>
2024-11-10 17:51:55 +0100

    implement optical rotary encoder subscriber

--- src/mmi_ros2_packages/mmi_package/mmi_package/rotary_subscriber.py
+++ src/mmi_ros2_packages/mmi_package/mmi_package/rotary_subscriber.py
@@ -1,31 +1,55 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import Int16
+from std_msgs.msg import Int16, Int32
 
 class RotarySubscriber(Node):
 
     def __init__(self):
         super().__init__('rotary_subscriber')
-        self.subscription = self.create_subscription(
+        self.rotary_subscription = self.create_subscription(
             Int16,
             '/esp/rotary_encoder',
-            self.listener_callback,
+            self.rotary_listener_callback,
             10
         )
-        self.previous_value = 0
-        self.rotation_direction = ''
-        self.subscription
+        self.rotary_previous_value = 0
+        self.rotary_rotation_direction = ''
+        self.rotary_subscription
 
-    def listener_callback(self, msg):
-        if msg.data > self.previous_value:
-            self.rotation_direction = "Right"
+
+
+        self.ore_subscription = self.create_subscription(
+            Int32,
+            'esp/optical_rotary_encoder',
+            self.ore_listener_callback,
+            10
+        )
+
+        self.ore_previous_value = 0
+        self.ore_rotation_direction = ''
+        self.ore_subscription
+
+
+    def rotary_listener_callback(self, msg):
+        if msg.data > self.rotary_previous_value:
+            self.rotary_rotation_direction = "Right"
         else:
-            self.rotation_direction = "Left"
+            self.rotary_rotation_direction = "Left"
         
-        self.get_logger().info('{direction} rotation - {value}'.format(direction = self.rotation_direction, value = msg.data))
+        self.get_logger().info('Rotary Encoder: {direction} rotation - {value}'.format(direction = self.rotary_rotation_direction, value = msg.data))
 
-        self.previous_value = msg.data
+        self.rotary_previous_value = msg.data
+
+    def ore_listener_callback(self, msg):
+        if msg.data > self.ore_previous_value:
+            self.ore_rotation_direction = "Right"
+        else:
+            self.ore_rotation_direction = "Left"
+        
+        self.get_logger().info('Optical Rotary Encoder: {direction} rotation - {value}'.format(direction = self.ore_rotation_direction, value = msg.data))
+
+        self.ore_previous_value = msg.data
 
 def main(args=None):
     rclpy.init(args=args)
